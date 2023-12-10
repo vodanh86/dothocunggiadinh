e8df1d0ffcf5dbcfc4e66bb1204576e22ea9703d
cuongdm <>
2023-12-10 21:54:33 +0700

    update name

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>Đồ thờ cúng</b> admin',
 
     /*
     |--------------------------------------------------------------------------
