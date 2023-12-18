95f445ba60134d88fedd7a3370e37718eaea5996
DungTQ <dungtq.work@gmail.com>
2023-12-19 00:37:38 +0700

    he thong phan phoi

--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('business', 'BusinessController@find');
 Route::get('branch', 'BranchController@find');
 
+//product group
 Route::get('product-group', 'ProductGroupController@find');
 Route::get('product-group/all', 'ProductGroupController@getAll');
 Route::get('product-group/get-all', 'ProductGroupController@getAllWithLimit');
 Route::get('product-group/get-by-id', 'ProductGroupController@getById');
 
+//category
 Route::get('category', 'CategoryController@find');
 Route::get('category/get-by-product-group', 'CategoryController@findByProductGroup');
 Route::get('category/all', 'CategoryController@getAll');
 Route::get('category/get-by-id', 'CategoryController@getById');
 
+//product
 Route::get('product', 'ProductController@find');
 Route::get('product/all', 'ProductController@getAll');
 Route::get('product/get-by-id', 'ProductController@getById');
@@ -37,14 +40,20 @@ Route::get('product/high-light-product', 'ProductController@listHighLight');
 Route::get('product/get-by-product-group/{id}', 'ProductController@getByProductGroup');
 Route::get('product/get-related-product/{id}', 'ProductController@relatedProduct');
 
+//events
 Route::get('events/coming-soon', 'EventController@listComingSoonEvents');
 Route::get('events/get-by-id/{id}', 'EventController@find');
 
+//news
 Route::get('news/list-latest-news', 'NewsController@latestNews');
 Route::get('news/get-by-id/{id}', 'NewsController@find');
 
+//system information
 Route::get('system-information/about-us', 'SystemInformationController@aboutUs');
 Route::get('system-information/contact', 'SystemInformationController@contactInformation');
+
+//delivery system
+Route::get('delivery-system/all', 'DeliverySystemController@getAllDeliverySystem');
 
 
 
